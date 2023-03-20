30615a5d2bc5e0993f29eaf87a83b71978db743c
WelshDan <dan.roberts78@gmail.com>
2023-03-20 22:35:13 +0000

    Add favicon and final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,14 @@ document.addEventListener("DOMContentLoaded", function() {
       })
       }
 
-      runGame("addition");
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
+
+    runGame("addition");
 })
 
 /**
@@ -23,6 +30,10 @@ document.addEventListener("DOMContentLoaded", function() {
  */
 
 function runGame(gameType){
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     /*Creates one random number between 1 and 25*/
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
